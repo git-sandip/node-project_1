d647250c58a4cb5d47e7eeed933de5d2124839cb
Sandip Sapkota <079bct076@ioepc.edu.np>
2023-11-02 19:20:43 +0545

    project inatilized and ejs setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/addBlog", (req, res) => {
+  res.render("AddBlog");
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server has been started on http://localhost:${port}`);
+});
