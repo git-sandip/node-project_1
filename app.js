e0c9bc57a16ee2f5c8574f8229377f6c4c95e2b2
Sandip Sapkota <079bct076@ioepc.edu.np>
2023-11-03 19:02:09 +0545

    connected mysql server and sequlize

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-
 app.set("view engine", "ejs");
 
+//database
+require("./model/index");
 app.get("/", (req, res) => {
   res.render("index");
 });
---
+++ model/blogModel.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const Blog = sequelize.define("blog", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return Blog;
+};
