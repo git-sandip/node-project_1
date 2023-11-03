80953e6421bc80b9c8b090eee95794800b2bee6c
Sandip Sapkota <079bct076@ioepc.edu.np>
2023-11-03 20:20:39 +0545

    blog created and stored at database

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 const express = require("express");
+const { blogs } = require("./model/index");
 const app = express();
 require("dotenv").config();
 app.set("view engine", "ejs");
 
-//database
+// database
 require("./model/index");
+
+//use requested data
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
 app.get("/", (req, res) => {
   res.render("index");
 });
 app.get("/addBlog", (req, res) => {
   res.render("AddBlog");
+});
+app.get("/about", (req, res) => {
+  res.render("About");
+});
+app.post("/createBlog", async (req, res) => {
+  await blogs.create({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  res.send({
+    status: 200,
+    msg: "Blog Created",
+    title: req.body.title,
+    description: req.body.description,
+  });
 });
 
 const port = process.env.PORT || 8000;
--- model/index.js
+++ model/index.js
@@ -31,7 +31,7 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 // importing model files
-db.users = require("./blogModel.js")(sequelize, DataTypes);
+db.blogs = require("./blogModel.js")(sequelize, DataTypes);
 
 db.sequelize.sync({ force: false }).then(() => {
   console.log("yes re-sync done");
